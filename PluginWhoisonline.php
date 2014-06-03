67b2f2e75703fadf67d56c17e55655f893a4820c
Alejandro Pedraza <alejandro.pedraza@gmail.com>
2014-06-03 16:24:44 -0500

    lang - replaced old comments marker /*T*/ with new one lang()

--- PluginWhoisonline.php
+++ PluginWhoisonline.php
@@ -5,10 +5,10 @@ require_once 'modules/clients/models/UserGateway.php';
 class PluginWhoisonline extends DashboardPlugin
 {
     /* plugin member vars used by ClientExec */
-    var $name = /*T*/"Who's Online"/*/T*/;
-    var $smallName = /*T*/"Online"/*/T*/;
+    var $name;
+    var $smallName;
 
-    var $description = /*T*/"This shows the users currently logged into your ClientExec."/*/T*/;
+    var $description;
     var $default = true; // plugin to be included with fresh installs
     var $cache = true;
     var $sidebarPlugin = true;
@@ -17,6 +17,13 @@ class PluginWhoisonline extends DashboardPlugin
 
     var $jsLibs  = array('plugins/dashboard/whoisonline/plugin.js');
     var $cssPages = array('plugins/dashboard/whoisonline/plugin.css,plugins/dashboard/whoisonline/assets/flags.css');
+
+    function __construct($user, $typeOfFetch = 1) {
+        $this->name = lang("Who's Online");
+        $this->smallName = lang("Online");
+        $this->description = lang("This shows the users currently logged into your ClientExec.");
+        parent::__construct($user,$typeOfFetch);
+    }
 
     //override the getPanel of DashboardPlugin as we do not want or have an index.phtml to output
     //we can just return html directly
